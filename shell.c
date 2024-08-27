15055b60391bfbff5412b53543ef198fc32ad7d7
mohammedabarh <abmohammed606@gmail.com>
2024-08-26 18:20:59 -0700

    shell

---
+++ shell.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * main - Serves as the entry point for the application
+ *
+ * Returns: 0 if the program completes successfully
+ *
+ */
+
+int main(void)
+{
+	while(1)
+	{
+		readcmd();
+	}
+
+	return (0);
+}
+
