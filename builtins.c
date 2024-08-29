897ad0fbab1c212aabf6cd76997d90b782e6fd53
mohammedabarh <abmohammed606@gmail.com>
2024-08-29 09:44:40 -0700

    updat builtins

--- builtins.c
+++ builtins.c
@@ -1,33 +1,45 @@
 #include "shell.h"
 
-/**
- * shell_cd - Changes the current directory
- * @args: Arguments (args[1] is the directory)
- *
- * Return: 1 on success, 0 otherwise
- */
 int shell_cd(char **args)
 {
-    if (args[1] == NULL)
+    char *dir = args[1];
+    char cwd[PATH_MAX];
+    char *home = getenv("HOME");
+
+    if (dir == NULL)
     {
-        fprintf(stderr, "Expected argument to \"cd\"\n");
+        dir = home;
+    }
+    else if (strcmp(dir, "-") == 0)
+    {
+        dir = getenv("OLDPWD");
+        if (dir == NULL)
+        {
+            fprintf(stderr, "cd: OLDPWD not set\n");
+            return 1;
+        }
+        printf("%s\n", dir);
+    }
+
+    if (chdir(dir) != 0)
+    {
+        perror("cd");
+        return 1;
+    }
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        setenv("OLDPWD", getenv("PWD"), 1);
+        setenv("PWD", cwd, 1);
     }
     else
     {
-        if (chdir(args[1]) != 0)
-        {
-            perror("Error");
-        }
+        perror("getcwd");
     }
+
     return 1;
 }
 
-/**
- * shell_help - Prints help information
- * @args: Arguments (not used)
- *
- * Return: Always returns 1
- */
 int shell_help(char **args)
 {
     int i;
@@ -35,7 +47,9 @@ int shell_help(char **args)
         "cd",
         "help",
         "exit",
-        "env"
+        "env",
+        "setenv",
+        "unsetenv"
     };
     (void)args;
 
@@ -43,7 +57,7 @@ int shell_help(char **args)
     printf("Type program names and arguments, and hit enter.\n");
     printf("The following are built in:\n");
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < 6; i++)
     {
         printf("  %s\n", builtin_str[i]);
     }
@@ -52,12 +66,6 @@ int shell_help(char **args)
     return 1;
 }
 
-/**
- * shell_exit - Exits the shell
- * @args: Arguments (not used)
- *
- * Return: Always returns 0
- */
 int shell_exit(char **args)
 {
     int status = 0;
@@ -69,12 +77,6 @@ int shell_exit(char **args)
     exit(status);
 }
 
-/**
- * shell_env - Prints the current environment
- * @args: Arguments (not used)
- *
- * Return: Always returns 1
- */
 int shell_env(char **args)
 {
     int i = 0;
@@ -85,5 +87,39 @@ int shell_env(char **args)
         printf("%s\n", environ[i]);
         i++;
     }
+    return 1;
+}
+
+int shell_setenv(char **args)
+{
+    if (args[1] == NULL || args[2] == NULL)
+    {
+        fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+        return 1;
+    }
+
+    if (setenv(args[1], args[2], 1) != 0)
+    {
+        fprintf(stderr, "Failed to set environment variable\n");
+        return 1;
+    }
+
+    return 1;
+}
+
+int shell_unsetenv(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+        return 1;
+    }
+
+    if (unsetenv(args[1]) != 0)
+    {
+        fprintf(stderr, "Failed to unset environment variable\n");
+        return 1;
+    }
+
     return 1;
 }
