b7309828b3d97093e464052fc6b2a9395de6aca4
mohammedabarh <abmohammed606@gmail.com>
2024-08-29 09:35:27 -0700

    updat shell_functions

--- shell_functions.c
+++ shell_functions.c
@@ -1,12 +1,6 @@
 #include "shell.h"
 
-/**
- * split_line - Splits a line into tokens
- * @line: The line to split
- *
- * Return: An array of tokens
- */
-char **split_line(char *line)
+char **split_line(char *line, char *delim)
 {
     int bufsize = BUFFER_SIZE, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
@@ -18,7 +12,7 @@ char **split_line(char *line)
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(line, TOKEN_DELIM);
+    token = strtok(line, delim);
     while (token != NULL)
     {
         tokens[position] = token;
@@ -35,18 +29,56 @@ char **split_line(char *line)
             }
         }
 
-        token = strtok(NULL, TOKEN_DELIM);
+        token = strtok(NULL, delim);
     }
     tokens[position] = NULL;
     return tokens;
 }
 
-/**
- * execute - Executes a command
- * @args: Array of arguments
- *
- * Return: 1 if the shell should continue running, 0 if it should terminate
- */
+char **split_logical_ops(char *line)
+{
+    int bufsize = BUFFER_SIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+    char *rest = line;
+
+    if (!tokens)
+    {
+        fprintf(stderr, "Allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((token = strtok_r(rest, "&|", &rest)))
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += BUFFER_SIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens)
+            {
+                fprintf(stderr, "Allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if (rest[0] == '&')
+        {
+            tokens[position++] = "&&";
+            rest++;
+        }
+        else if (rest[0] == '|')
+        {
+            tokens[position++] = "||";
+            rest++;
+        }
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
 int execute(char **args)
 {
     int i;
@@ -54,19 +86,23 @@ int execute(char **args)
         "cd",
         "help",
         "exit",
-        "env"
+        "env",
+        "setenv",
+        "unsetenv"
     };
     int (*builtin_func[]) (char **) = {
         &shell_cd,
         &shell_help,
         &shell_exit,
-        &shell_env
+        &shell_env,
+        &shell_setenv,
+        &shell_unsetenv
     };
 
     if (args[0] == NULL)
         return 1;
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < 6; i++)
     {
         if (strcmp(args[0], builtin_str[i]) == 0)
             return (*builtin_func[i])(args);
@@ -75,12 +111,6 @@ int execute(char **args)
     return launch(args);
 }
 
-/**
- * launch - Launches a program
- * @args: Array of arguments
- *
- * Return: Always returns 1, to continue execution
- */
 int launch(char **args)
 {
     pid_t pid;
@@ -90,7 +120,6 @@ int launch(char **args)
     pid = fork();
     if (pid == 0)
     {
-        /* Child process */
         command_path = get_location(args[0]);
         if (command_path == NULL)
         {
@@ -106,12 +135,10 @@ int launch(char **args)
     }
     else if (pid < 0)
     {
-        /* Error forking */
         perror("Error:");
     }
     else
     {
-        /* Parent process */
         do {
             waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
@@ -120,12 +147,6 @@ int launch(char **args)
     return 1;
 }
 
-/**
- * get_location - Gets the location of a command
- * @command: The command to locate
- *
- * Return: The full path of the command, or NULL if not found
- */
 char *get_location(char *command)
 {
     char *path, *path_copy, *path_token, *file_path;
